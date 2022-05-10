8207015dc3972c317db375a0ff7776c2e31c3a74
Yung-Tien Lin <yungtlin@g.ucla.edu>
2022-05-09 21:10:15 -0700

    Added a couple candidate functions to gen_data

--- ML_model/training_data/gen_data.py
+++ ML_model/training_data/gen_data.py
@@ -11,7 +11,7 @@ import numpy as np
 # return x_b and x_f
 def average_location(x, r):
     x_b = (x[:-1] + x[1:])/2 # mid points
-    x_f = x_b[r+1: -r] # discards unusable flux points near boundaries
+    x_f = x_b[r: -(r-1)] # discards unusable flux points near boundaries
     return x_b, x_f
 
 ### Data Xy generation ###
@@ -37,14 +37,14 @@ def f_int2b(F_b, x_b):
 
 # pack sequntial data into a 2d data package
 def packto2D(f_b, h, f_h, r):
-    stencil = 2*r + 1
+    stencil = 2*r - 1
     ndata = f_h.shape[0]
     
     data_f_b = np.zeros((ndata, stencil))
     for i in range(stencil):
         data_f_b[:, i] = f_b[i:i+ndata]
 
-    data_h = h[r:-r].reshape((-1, 1))
+    data_h = h[(r-1):-(r-1)].reshape((-1, 1))
 
     data_X = np.append(data_f_b, data_h, axis=1)
     data_y = f_h.reshape((-1, 1))
@@ -80,11 +80,11 @@ def check_f(x, r, func, args, n_exact=1001):
 # F_b: F at x_b (F = \int f)
 # f_h: \hat{f} = flux on edge (x_{j+1/2})
 
-# f = sin(omega*(x - phi))
+# f = sin(omega*(x - delta))
 def f_sin(x_b, x_f, args):
-    (omega, phi) = args
-    f_h = np.sin(omega*(x_f - phi))
-    F_b = -np.cos(omega*(x_b - phi))/omega
+    (omega, delta) = args
+    f_h = np.sin(omega*(x_f - delta))
+    F_b = -np.cos(omega*(x_b - delta))/omega
     
     return F_b, f_h
 
@@ -96,20 +96,49 @@ def f_step(x_b, x_f, args):
 
     return F_b, f_h
 
+# f = (k*(x_f-delta))%1 -1/2 symmetry sawtooth \in [-1, 1] 
+def f_sawtooth(x_b, x_f, args):
+    k, delta = args
+    f_h = (2*k*(x_f-delta+0.5))%2 - 1
+    
+    dx = (2*k*(x_b-delta+0.5))%2 - 1
+    F_b = 1/4*dx**2
 
+    return F_b, f_h
 
+# f = tanh(k*x)
+def f_tanh(x_b, x_f, args):
+    k = args
+    f_h = np.tanh(k*x_f)
+    F_b = np.log(np.cosh(k*x_b))/k
+
+    return F_b, f_h
+
+# f = (x - delta)**n
+def f_poly(x_b, x_f, args):
+    n, delta = args
+    f_h = (x_f - delta)**n
+    F_b = ((x_b - delta)**(n+1))/(n+1)
+
+    return F_b, f_h
+
+# testing field
 if __name__ == "__main__":
-    r = 2 # optimal accuracy/stencil: 2r+1
+    r = 3 # optimal accuracy/stencil: 2r-1
 
-    L_l = 0 # left boundary
+    L_l = -1 # left boundary
     L_r = 1 # right boundary
-    nx = 101
+    nx = 18
     x = np.linspace(L_l, L_r, nx)
 
-    func = f_step
-    delta = 0.5
-    args = (delta)
+    func = f_poly
+    n = 5
+    delta = 0
+    args = (n, delta)
 
-    #check_f(x, r, func, args)
-    #X, y = gen_Xy(x, r, func, args)
+    x_b, x_f = average_location(x, r)
+    F_b, f_h = func(x_b, x_f, args)
+    check_f(x, r, func, args)
+    
+    X, y = gen_Xy(x, r, func, args)
 
