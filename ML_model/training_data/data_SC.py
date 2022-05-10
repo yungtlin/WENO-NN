365ef1cbedbbcc5b8627cc8af80b32b0e9b03dbd
Yung-Tien Lin <yungtlin@g.ucla.edu>
2022-05-09 22:03:01 -0700

    Created data_SC

---
+++ ML_model/training_data/data_SC.py
@@ -0,0 +1,20 @@
+# Generates the data from the method of Stevens and Colonius
+# Check WENO-NN report for technical details
+
+### Libraries ###
+import numpy as np  
+import gen_data
+
+### SC ####
+def data_SC(path):
+    pass
+
+# testing field
+if __name__ == "__main__":
+    # saving path
+    folder = ""
+    file = "data_SC.npy"
+    path = folder + file
+
+    # executing data generation process
+    data_SC(path)
