583ece936494079cbd9321b6221c676286282c42
Yung-Tien Lin <yungtlin@g.ucla.edu>
2022-05-11 17:01:22 -0700

    Completed the initial implementation of data_SC.py

--- ML_model/training_data/data_SC.py
+++ ML_model/training_data/data_SC.py
@@ -76,20 +76,54 @@ def mix_sin_stp(x, r, args):
 
     return F_b, f_h
 
+# sin + saw
+# f = alph*sin + saw
+def mix_sin_sw(x, r, args):
+    alpha, args_sin, args_ssw = args
+    F_bsin, f_hsin = gen_data.f_sin(x, r, args_sin)
+    F_bsw, f_hsw = sign_sawtooth(x, r, args_ssw)
+
+    F_b = alpha*F_bsin + F_bsw
+    f_h = alpha*f_hsin + f_hsw
+
+    return F_b, f_h
+
+# sin + tanh
+# f = alph*sin + tanh
+def mix_sin_tanh(x, r, args):
+    alpha, args_sin, args_stanh = args
+    F_bsin, f_hsin = gen_data.f_sin(x, r, args_sin)
+    F_btanh, f_htanh = sign_tanh(x, r, args_stanh)
+
+    F_b = alpha*F_bsin + F_btanh
+    f_h = alpha*f_hsin + f_htanh
+
+    return F_b, f_h
+
 
 ### SC ####
 def data_SC(path):
     r = 3 # WENO5 based
-    r_min = 1e-3 # data minimum distance
+    r_min = 3e-3 # data minimum distance
 
     dataU = init_data_U(r, r_min)
+
+    data_SC_sin(dataU, r)       # ~11,000 sin      (11,000)
+    data_SC_sw(dataU, r)        # ~8,000  sawtooth (19,000)
+    data_SC_step(dataU, r)      # ~1,000  step     (20,000)
+    data_SC_tanh(dataU, r)      # ~5,000  tanh     (25,000)
+    data_SC_sin_stp(dataU, r)   # ~10,000  sin+stp (35,000)
+    data_SC_sin_sw(dataU, r)    # ~12,000 sin+saw  (47,000)
+    data_SC_sin_tanh(dataU, r)  # ~34,000 sin+tanh (81,000)
+
+    X_all = dataU.X
+    print("Data generation completed! (total: %i)\n"%(X_all.shape[0]))
+
+    with open(path, "wb") as f:
+        print("Saving data: %s"%path)
+        np.save(f, X_all)
     
-    
-    data_SC_sin(dataU, r) # ~17,000 # sin
-    data_SC_sw(dataU, r) # ~9,000 # sawtooth (26,000)
-    data_SC_step(dataU, r) # ~1,000 # step (27,000)
-    data_SC_tanh(dataU, r) # ~8,000 # tanh (34,000)
-    data_SC_sin_stp(dataU, r) # ~22,000 sin + step (22,000)
+
 
 # randomly add sinusoidal function initialize data_U
 def init_data_U(r, r_min):
@@ -136,7 +170,6 @@ def data_SC_sin(dataU, r):
             X_pack = np.append(X_pack, X, axis=0)
         dataU.add(X_pack)
 
-
 ## Sawtooth ##
 def data_SC_sw(dataU, r):
     func = sign_sawtooth
@@ -165,6 +198,7 @@ def data_SC_sw(dataU, r):
                 X = gen_X_sc(x, r, func, args)
                 X_pack = np.append(X_pack, X, axis=0)
             dataU.add(X_pack)
+
 ## Step ##
 def data_SC_step(dataU, r):
     func = sign_step
@@ -227,7 +261,7 @@ def data_SC_sin_stp(dataU, r):
     x = np.linspace(-0.5*L, 0.5*L, n_data+2*r)
     dx = np.mean(x[1:] - x[:-1])/2
 
-    n_alpha = 20
+    n_alpha = 10
     n_k = 5
     n_delta = 5
     n_shift = 5
@@ -251,7 +285,6 @@ def data_SC_sin_stp(dataU, r):
                 delta = L/k*np.random.random_sample()
                 args_sin = (omega, delta)
 
-
                 for sign in [1, -1]:            
                     for count_shift in range(n_shift):
                         shift = 2*dx*np.random.random_sample() - dx
@@ -262,12 +295,122 @@ def data_SC_sin_stp(dataU, r):
                         X_pack = np.append(X_pack, X, axis=0)
             dataU.add(X_pack)
 
+# Sinusoidal + Step
+# f = alph*sin + step
+def data_SC_sin_sw(dataU, r):
+    func = mix_sin_sw
+    L = 1
+    n_data = 5
+    x = np.linspace(-1*L, 0*L, n_data+2*r)
+    dx = np.mean(x[1:] - x[:-1])/2
+
+    n_alpha = 10
+    n_omega = 5
+    n_delta = 6
+    n_k = 2
+    n_shift = 2
+
+    for count_alpha in range(n_alpha):
+        alp_max = 1
+        alp_min = -3
+        exp_alp = (alp_max-alp_min)*np.random.random_sample() + alp_min 
+        alpha = np.exp(exp_alp)
+        
+        for count_omega in range(n_omega):
+            k_sin_max = 1
+            k_sin_min = -1
+            exp_k_sin = (k_sin_max-k_sin_min)*np.random.random_sample() + k_sin_min 
+            k_sin = np.exp(exp_k_sin)
+            
+            X_pack = np.zeros((0, 6))
+            print("sin + sawtooth (alpha:%.5f, k:%.5f):"%(alpha, k_sin))
+            for count_delta in range(n_delta):
+                omega = 2*np.pi*k_sin
+                delta = L/k_sin*np.random.random_sample()
+                args_sin = (omega, delta)
+    
+                for sign in [1, -1]:
+                    for count_k in range(n_k):
+                        k_sw_max = 0.7
+                        k_sw_min = -2
+                        exp_k_sw = (k_sw_max-k_sw_min)*np.random.random_sample() + k_sw_min 
+                        k_sw = np.exp(exp_k_sw)
+
+                        for count_shift in range(n_shift):
+                            shift = 2*dx*np.random.random_sample() - dx
+                            args_ssw = (sign, (k_sw, shift)) 
+                
+                            args = (alpha, args_sin, args_ssw)
+                            X = gen_X_sc(x, r, func, args)
+                            X_pack = np.append(X_pack, X, axis=0)
+            dataU.add(X_pack)
+
+# Sinusoidal + Hyperbolic Tangent
+# f = alph*sin + tanh
+def data_SC_sin_tanh(dataU, r):
+    func = mix_sin_tanh
+
+    L = 1
+    n_data = 5
+    x_base = np.linspace(-0.5*L, 0.5*L, n_data+2*r)
+    dx = np.mean(x_base[1:] - x_base[:-1])/2
+
+    n_alpha = 30
+    n_omega = 5
+    n_delta = 6
+    n_k = 2
+    n_shift = 2
+
+    for count_alpha in range(n_alpha):
+        alp_max = 1
+        alp_min = -3
+        exp_alp = (alp_max-alp_min)*np.random.random_sample() + alp_min 
+        alpha = np.exp(exp_alp)
+
+        for count_omega in range(n_omega):
+            k_sin_max = 1.6
+            k_sin_min = -1
+            exp_k_sin = (k_sin_max-k_sin_min)*np.random.random_sample() + k_sin_min 
+            k_sin = np.exp(exp_k_sin)
+            
+            X_pack = np.zeros((0, 6))
+            print("sin + tanh (alpha:%.5f, k:%.5f):"%(alpha, k_sin))
+            for count_delta in range(n_delta):
+                omega = 2*np.pi*k_sin
+                delta = L/k_sin*np.random.random_sample()
+                args_sin = (omega, delta)
+
+                for sign in [1, -1]:
+                    for count_k in range(n_k):
+                        k_tanh_max = 4
+                        k_tanh_min = 0
+                        exp_k_tanh = (k_tanh_max-k_tanh_min)*np.random.random_sample() + k_tanh_min 
+                        k_tanh = np.exp(exp_k_tanh)
+
+                        for count_shift in range(n_shift):
+                            shift = 2*dx*np.random.random_sample() - dx
+
+                            x = x_base + shift
+                            args_stanh = (sign, (k_tanh))
+                            
+                            args = (alpha, args_sin, args_stanh)
+
+                            X = gen_X_sc(x, r, func, args)
+                            X_pack = np.append(X_pack, X, axis=0)
+            dataU.add(X_pack)
+
 # testing field
 if __name__ == "__main__":
     # saving path
     folder = ""
-    file = "data_SC.npy"
+    file = "data_SC_0.npy"
     path = folder + file
 
     # executing data generation process
     data_SC(path)
+
+    # test by loading data
+    #with open(path, "rb") as f:
+    #    print("Loading data: %s"%path)
+    #    X = np.load(f)
+    #print(X.shape)
